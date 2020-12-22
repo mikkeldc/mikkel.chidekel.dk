a596d791420ab363c26401621a0487bc8b4bacc8
mikkeldc <mikkdc@gmail.com>
2020-12-22 21:36:37 +0100

    add test webside

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const thankyouMessage = "tak fordi du trykkede";
+const confirmClick = "se der er klikket";
+
+
+console.log("javascript er ikke hjemme");
+
+
+function clickMessage() {
+    alert(thankyouMessage);
+    console.log(confirmClick)
+}
